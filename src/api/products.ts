295bfc2d43ad144057e28ae0a56672ec2b9baed1
Grey Boolean <greyboolean@gmail.com>
2024-06-07 20:09:44 +0530

    refactor: convert products.ts to typescript

---
+++ src/api/products.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { Product } from "../types/product";
+
+export const getAllProducts = async (): Promise<Product[] | null> => {
+	try {
+		const response = await axios.get(`https://fakestoreapi.com/products`);
+		// if (response.status === 200) {
+		// 	return response.data.filter(
+		// 		(product: Product) =>
+		// 			product.category === "men's clothing" ||
+		// 			product.category === "women's clothing"
+		// 	);
+		// }
+		return response.data.filter(
+			(product: Product) =>
+				product.category === "men's clothing" ||
+				product.category === "women's clothing"
+		);
+	} catch (error) {
+		return null;
+	}
+};
+
+export const getProductsByCategory = async (
+	category: string
+): Promise<Product[] | null> => {
+	try {
+		const response = await axios.get(
+			`https://fakestoreapi.com/products/category/${
+				category === "men" ? "men's%20clothing" : "women's%20clothing"
+			}`
+		);
+		// if (response.status === 200) {
+		// 	return response.data;
+		// }
+		return response.data;
+	} catch (error) {
+		return null;
+	}
+};
