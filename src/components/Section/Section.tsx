a339bcdfa0470e738043cda7a193709dca41fabc
Grey Boolean <greyboolean@gmail.com>
2024-06-07 11:47:20 +0530

    refactor section to typescript

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,7 +1,14 @@
+import { ReactNode } from "react";
 import Loading from "../Loading/Loading";
 import styles from "./Section.module.css";
 
-function Section({ heading, children, loading }) {
+type SectionProps = {
+	heading: string;
+	loading: boolean;
+	children: ReactNode
+}
+
+function Section({ heading, loading, children }: SectionProps) {
 	return (
 		<div className={styles.section}>
 			<h1 className={styles.heading}>{heading}</h1>
