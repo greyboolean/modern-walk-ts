91c252a122b5af9b315a86e93f53ba945446bfb0
Grey Boolean <greyboolean@gmail.com>
2024-06-13 10:22:02 +0530

    refactor: move Layout to layouts

---
+++ src/ui-core/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import OutletContainer from "../../components/OutletContainer/OutletContainer";
+
+function Layout() {
+	return (
+		<div>
+			<Header />
+			<OutletContainer />
+		</div>
+	);
+}
+
+export default Layout;
