0d8ed300f4330baa9127ed425d75b1e877cafb62
Grey Boolean <greyboolean@gmail.com>
2024-06-18 16:00:52 +0530

    feat: hide watchlist button if user not signed in

--- src/ui-core/components/molecules/Navbar/Navbar.component.tsx
+++ src/ui-core/components/molecules/Navbar/Navbar.component.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import { UserAuth } from "../UserAuth";
-import { Button } from "../../atoms/Button"; 
+import { Button } from "../../atoms/Button";
 import styles from "./Navbar.module.css";
+import { SignedIn } from "@clerk/clerk-react";
 
 function Navbar() {
 	return (
 		<nav className={styles.navbar}>
+			<SignedIn>
+				<Link to="/watchlist">
+					<Button>Watchlist</Button>
+				</Link>
+			</SignedIn>
 			<UserAuth />
-			<Link to="/watchlist">
-				<Button>Watchlist</Button>
-			</Link>
 		</nav>
 	);
 }
